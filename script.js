24feb40ee3dba372538306afe2ebf6aca425f24f
Bisma Bendesa <bismabendesajaya@gmail.com>
2022-04-24 11:43:05 +0800

    - Create new localstorage key LOCAL_STORAGE_SELECTED_LIST_ID_KEY - Create variable selectedListId contains getItem from the selected list id key - Add click event to the parentLists to setup the selectedListId variable - Add click event the deleteListBtn to delete the current selected list - Add the new localStorage key into the save function - Add if statement to the render function. if the lists.id equal to selectedListId, we want to add active-list class on that element

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const parentLists = document.querySelector("[data-lists]");
 const listForm = document.querySelector("[data-new-list-form]");
 const listInput = document.querySelector("[data-new-list-input]");
+const deleteListBtn = document.querySelector("[data-delete-list]");
 
 const LOCAL_STORAGE_LIST_KEY = "task.lists";
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = "task.selectedListId";
 
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
 
 listForm.addEventListener("submit", (e) => {
@@ -19,18 +22,30 @@ listForm.addEventListener("submit", (e) => {
   }
 });
 
+parentLists.addEventListener("click", (e) => {
+  if (e.target.tagName.toLowerCase() === "li") {
+    selectedListId = e.target.dataset.listId;
+    saveAndRender();
+  }
+});
+
+deleteListBtn.addEventListener("click", (e) => {
+  lists = lists.filter((list) => list.id !== selectedListId);
+  saveAndRender();
+});
+
 const createList = (listName) => {
   return { id: Date.now().toString(), taskName: listName, tasks: [] };
 };
 
 function saveAndRender() {
-  console.log(Date.now());
   save();
   render();
 }
 
 function save() {
   localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
 }
 
 function render() {
@@ -39,6 +54,9 @@ function render() {
     const li = document.createElement("li");
     li.innerText = list.taskName;
     li.dataset.listId = list.id;
+    if (list.id === selectedListId) {
+      li.classList.add("active-list");
+    }
     li.classList.add("list-name");
     parentLists.append(li);
   });
