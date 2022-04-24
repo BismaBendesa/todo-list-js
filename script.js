a3f28745e89d6a029c1bad5b805a0f0a9f56ab72
Bisma Bendesa <bismabendesajaya@gmail.com>
2022-04-24 10:32:43 +0800

    Create createElement() and add submit event listener to the form submit

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 const parentLists = document.querySelector("[data-lists]");
+const listForm = document.querySelector("[data-new-list-form]");
+const listInput = document.querySelector("[data-new-list-input]");
 
 let lists = [
-  { id: 1, taskName: "Shitting" },
-  { id: 2, taskName: "Twerking" },
+  { id: 1, taskName: "Shitting", tasks: [] },
+  { id: 2, taskName: "Twerking", tasks: [] },
 ];
+
+const createList = (listName) => {
+  return { id: Date.now().toString(), taskName: listName, tasks: [] };
+};
+
+listForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const listName = listInput.value;
+  if (listName == null || listName === "" || listName === undefined) {
+    return;
+  } else {
+    const list = createList(listName);
+    listInput.value = null;
+    lists.push(list);
+    render();
+  }
+});
 
 function render() {
   clearElement(parentLists);
@@ -11,6 +30,7 @@ function render() {
     const li = document.createElement("li");
     li.innerText = list.taskName;
     li.dataset.listId = list.id;
+    console.log(list.taskName);
     li.classList.add("list-name");
     parentLists.append(li);
   });
